11f54ee106850cbefa1250d2d0bd2cedbd991e5a
Ole4961 <marius.torvmark@gmail.com>
2023-02-02 16:26:11 +0100

    Added more get requests, fixed problem where it wouldn't read from Google Books' Api

--- Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.APICommunicatorService/BooksDataTransferObject.cs
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.APICommunicatorService/BooksDataTransferObject.cs
@@ -2,7 +2,7 @@ namespace Hiof.DotNetCourse.V2023.Group14.APICommunicatorService
 {
     public class BooksDataTransferObject
     {
-        public string? ISBN{ get; set; }
+        public int ISBN{ get; set; }
         public string? Title{ get; set; }  
         public string? Author { get; set; }
     }
--- Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.APICommunicatorService/Controllers/BookController.cs
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.APICommunicatorService/Controllers/BookController.cs
@@ -17,16 +17,53 @@ namespace Hiof.DotNetCourse.V2023.Group14.APICommunicatorService.Controllers
             _logger = logger;
         }
 
-        [HttpGet("v1/volumes/")]
-        public async Task<object> Get(string q)
+        [HttpGet("GetBookISBN")]
+        public async Task<string> Get(string ISBN)
         {
             using var client = new HttpClient();
-            var url = "https://www.googleapis.com/books/v1/volumes?q=search";
+            var url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + ISBN;
             HttpResponseMessage responseMessage = await client.GetAsync(url);
             responseMessage.EnsureSuccessStatusCode();
             var responseBody = await responseMessage.Content.ReadAsStringAsync();
 
             return responseBody;
         }
+
+        [HttpGet("GetBookTitle")]
+        public async Task<string> GetByTitle(string Title)
+        {
+            using var client = new HttpClient();
+            var url = "https://www.googleapis.com/books/v1/volumes?q=intitle:" + Title;
+            HttpResponseMessage responseMessage = await client.GetAsync(url);
+            responseMessage.EnsureSuccessStatusCode();
+            var responseBody = await responseMessage.Content.ReadAsStringAsync();
+
+            return responseBody;
+        }
+
+        [HttpGet("GetBookAuthor")]
+        public async Task<string> GetByAuthors(string Authors)
+        {
+            using var client = new HttpClient();
+            var url = "https://www.googleapis.com/books/v1/volumes?q=inauthor:" + Authors;
+            HttpResponseMessage responseMessage = await client.GetAsync(url);
+            responseMessage.EnsureSuccessStatusCode();
+            var responseBody = await responseMessage.Content.ReadAsStringAsync();
+
+            return responseBody;
+        }
+
+        [HttpGet("GetBookCategories")]
+        public async Task<string> GetBySubject(string Subject)
+        {
+            using var client = new HttpClient();
+            var url = "https://www.googleapis.com/books/v1/volumes?q=categories:" + Subject;
+            HttpResponseMessage responseMessage = await client.GetAsync(url);
+            responseMessage.EnsureSuccessStatusCode();
+            var responseBody = await responseMessage.Content.ReadAsStringAsync();
+
+            return responseBody;
+        }
+
     }
 }
