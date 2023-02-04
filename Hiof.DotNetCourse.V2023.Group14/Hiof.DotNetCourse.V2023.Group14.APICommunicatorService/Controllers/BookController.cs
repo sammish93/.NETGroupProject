1f1e93ef5cc62a65413013682c1559524afdeb83
stian96 <stiankru@hiof.no>
2023-02-04 13:33:57 +0100

    Made an GetUrl method which take two parameters (search and endparameter).

--- Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.APICommunicatorService/Controllers/BookController.cs
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.APICommunicatorService/Controllers/BookController.cs
@@ -6,7 +6,7 @@ namespace Hiof.DotNetCourse.V2023.Group14.APICommunicatorService.Controllers
     [Route("books")]
     public class BookController : ControllerBase
     {
-        private static BooksDataTransferObject books = new();
+        private static readonly BooksDataTransferObject books = new();
 
         // Changed client to a global variable instead
         private static readonly HttpClient client = new();
@@ -22,7 +22,7 @@ namespace Hiof.DotNetCourse.V2023.Group14.APICommunicatorService.Controllers
         [HttpGet("GetBookISBN")]
         public async Task<string> Get(string ISBN)
         {
-            var url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + ISBN;
+            var url = GetUrl("isbn", ISBN);
             HttpResponseMessage responseMessage = await client.GetAsync(url);
             responseMessage.EnsureSuccessStatusCode();
             var responseBody = await responseMessage.Content.ReadAsStringAsync();
@@ -33,7 +33,7 @@ namespace Hiof.DotNetCourse.V2023.Group14.APICommunicatorService.Controllers
         [HttpGet("GetBookTitle")]
         public async Task<string> GetByTitle(string Title)
         {
-            var url = "https://www.googleapis.com/books/v1/volumes?q=intitle:" + Title;
+            var url = GetUrl("intitle", Title);
             HttpResponseMessage responseMessage = await client.GetAsync(url);
             responseMessage.EnsureSuccessStatusCode();
             var responseBody = await responseMessage.Content.ReadAsStringAsync();
@@ -44,7 +44,7 @@ namespace Hiof.DotNetCourse.V2023.Group14.APICommunicatorService.Controllers
         [HttpGet("GetBookAuthor")]
         public async Task<string> GetByAuthors(string Authors)
         {
-            var url = "https://www.googleapis.com/books/v1/volumes?q=inauthor:" + Authors;
+            var url = GetUrl("inauthor", Authors);
             HttpResponseMessage responseMessage = await client.GetAsync(url);
             responseMessage.EnsureSuccessStatusCode();
             var responseBody = await responseMessage.Content.ReadAsStringAsync();
@@ -55,12 +55,18 @@ namespace Hiof.DotNetCourse.V2023.Group14.APICommunicatorService.Controllers
         [HttpGet("GetBookCategories")]
         public async Task<string> GetBySubject(string Subject)
         {
-            var url = "https://www.googleapis.com/books/v1/volumes?q=categories:" + Subject;
+            var url = GetUrl("categories", Subject);
             HttpResponseMessage responseMessage = await client.GetAsync(url);
             responseMessage.EnsureSuccessStatusCode();
             var responseBody = await responseMessage.Content.ReadAsStringAsync();
 
             return responseBody;
+        }
+
+        // Returns URL for googleapis with specified search and endparameter
+        private static String GetUrl(string search, string parameter)
+        {
+            return $"https://www.googleapis.com/books/v1/volumes?q={search}:" + parameter;
         }
 
     }
