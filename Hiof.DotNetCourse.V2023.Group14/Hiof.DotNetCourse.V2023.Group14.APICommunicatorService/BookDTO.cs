cf5b8f6dfef58b9ac0459a12c8596d2e1838e37e
stian96 <stiankru@hiof.no>
2023-02-05 18:17:00 +0100

    Added some more tests

--- Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.APICommunicatorService/BookDTO.cs
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.APICommunicatorService/BookDTO.cs
@@ -6,7 +6,7 @@ namespace Hiof.DotNetCourse.V2023.Group14.APICommunicatorService
         public string? Title{ get; set; }  
         public string? Author { get; set; }
         public string? Publisher { get; set; }
-        public int PageCount { get; set; }
+        public int pageCount { get; set; }
         public string? PrintType { get; set; }
         public string? Categories { get; set; }
         public string? Language { get; set; }
