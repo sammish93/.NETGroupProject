752c74ce855c8a1696d0e082511abffb12fee963
a5h71 <ashtim@hiof.no>
2023-02-09 13:44:52 +0100

    made changes to the route

--- Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.UserAccountService/Controllers/UserAccountController.cs
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.UserAccountService/Controllers/UserAccountController.cs
@@ -5,16 +5,20 @@ using Microsoft.AspNetCore.Mvc;
 namespace Hiof.DotNetCourse.V2023.Group14.UserAccountService.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
-    public class UserAccountController : Controller
+    [Route("users")]
+    public class UserAccountController : ControllerBase
     {
         private readonly UserAccountContext _userAccountContext;
 
-        public UserAccountController(UserAccountContext userAccountContext)
+        public UserAccountController(UserAccountContext userAccountContext )
         {
             _userAccountContext = userAccountContext;
         }
-        [HttpGet]
+
+        // Example of inserting a new object into the database. If you use swagger you can see that we supply it with a JSON DTO.
+        // This Http request isn't coded to include lots of different Http codes yet.
+        // Remember that it's important that this is set to async, along with await keywords.
+        [HttpPost]
         public async Task<ActionResult> Create(User user)
         {
             await _userAccountContext.Users.AddAsync(user);
