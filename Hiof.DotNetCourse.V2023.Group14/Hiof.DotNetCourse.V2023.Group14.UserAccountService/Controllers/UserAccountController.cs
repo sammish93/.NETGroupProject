78bf127fd68be605329dc47fa6af49fc6c8558f0
a5h71 <ashtim@hiof.no>
2023-02-09 10:12:59 +0100

    created new controller and UserAccountContext

---
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.UserAccountService/Controllers/UserAccountController.cs
@@ -0,0 +1,6 @@
+﻿namespace Hiof.DotNetCourse.V2023.Group14.UserAccountService.Controllers
+{
+    public class UserAccount
+    {
+    }
+}
---
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.UserAccountService/Data/UserAccountContext.cs
@@ -0,0 +1,31 @@
+﻿using Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Hiof.DotNetCourse.V2023.Group14.UserAccountService.Data
+{
+    public class DbOrmUserAccountContext : DbContext
+    {
+        public DbOrmUserAccountContext(DbContextOptions<DbOrmUserAccountContext> dbContextOptions) : base(dbContextOptions) 
+        {
+            try
+            {
+                var databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+                if (databaseCreator != null)
+                {
+                    if (!databaseCreator.CanConnect()) databaseCreator.Create();
+                    if (!databaseCreator.HasTables()) databaseCreator.CreateTables();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+        public DbSet<User> Users { get; set; }
+    }
+}
