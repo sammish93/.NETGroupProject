fce96058b15134f64484d5691c61890e055e9b32
a5h71 <ashtim@hiof.no>
2023-02-09 13:05:43 +0100

    Fixed the build issue and updated other classes

--- Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.UserAccountService/Controllers/UserAccountController.cs
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.UserAccountService/Controllers/UserAccountController.cs
@@ -6,8 +6,20 @@ namespace Hiof.DotNetCourse.V2023.Group14.UserAccountService.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class UserAccountController : ControllerBase
+    public class UserAccountController : Controller
     {
-        private readonly UserAccountContext
+        private readonly UserAccountContext _userAccountContext;
+
+        public UserAccountController(UserAccountContext userAccountContext)
+        {
+            _userAccountContext = userAccountContext;
+        }
+        [HttpGet]
+        public async Task<ActionResult> Create(User user)
+        {
+            await _userAccountContext.Users.AddAsync(user);
+            await _userAccountContext.SaveChangesAsync();
+            return Ok();
+        }
     }
 }
---
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.UserAccountService/ProgramUserAccount.cs
@@ -0,0 +1,50 @@
+﻿
+using Hiof.DotNetCourse.V2023.Group14.UserAccountService.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hiof.DotNetCourse.V2023.Group14.UserAccountService
+{
+    public class ProgramUserAccount
+    {
+        public static void Main(string[] args) 
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+
+            var dbHost = "Server=tcp:dotnetgroupproject.database.windows.net,1433";
+            var dbName = "DotNetGroupProjectSQLDB";
+            var dbPwd = "Hodgeheg14";
+            var dbConnectionStr = $"{dbHost};Initial Catalog={dbName};Persist Security Info=False;User ID=hedgehogfans;Password={dbPwd};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            builder.Services.AddDbContext<UserAccountContext>(options => options.UseSqlServer(dbConnectionStr));
+            
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            
+            
+
+            var app = builder.Build();
+
+            
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            
+
+            app.UseHttpsRedirection();
+            
+            app.UseAuthorization();
+
+            app.MapControllers();
+            
+            app.Run();
+
+
+        }
+    }
+}
+
