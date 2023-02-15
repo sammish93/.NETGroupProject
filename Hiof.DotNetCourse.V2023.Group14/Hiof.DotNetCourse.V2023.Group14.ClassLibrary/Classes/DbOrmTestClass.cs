722134dbb472859932f776df12628da0e66ec624
Sam Davies <samuelcd@hiof.no>
2023-02-15 15:16:12 +0100

    wrote documentation on how to install a local database, add and update migrations etc

--- Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.ClassLibrary/Classes/DbOrmTestClass.cs
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.ClassLibrary/Classes/DbOrmTestClass.cs
@@ -24,5 +24,7 @@ namespace Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes
         public string Name { get; set; }
         [Column("age")]
         public int Age { get; set; }
+
+        public DateTime lastActive { get; set; }
     }
 }
