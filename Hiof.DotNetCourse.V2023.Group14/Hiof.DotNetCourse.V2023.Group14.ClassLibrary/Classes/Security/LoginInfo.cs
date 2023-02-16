8be9eab3ade2bbe10d1875a94d6bf24418067754
Sam Davies <samuelcd@hiof.no>
2023-02-16 10:05:37 +0100

    removed id field from LoginInfo

--- Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.ClassLibrary/Classes/Security/LoginInfo.cs
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.ClassLibrary/Classes/Security/LoginInfo.cs
@@ -6,18 +6,15 @@ namespace Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Security
 	// Class used to verify users in Login Service.
 	public class LoginInfo
 	{
-		//private readonly int _id;
 		private string _username;
 		private string _password;
 
         public LoginInfo(string username, string password)
 		{
-			//_id = id;
 			_username = username;
 			_password = password;
 		}
 
-		//public int Id { get => _id; }
         public string UserName { get => _username; set => _username = value; }
 		public string Password { get => _password; set => _password = value; }
 
