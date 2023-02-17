7b7a9e71d92280995132dcb9dcfdf027584d6dd6
stian96 <stiankru@hiof.no>
2023-02-17 18:28:53 +0100

    added a new test class

--- Hiof.DotNetCourse.V2023.Group14/APICommunicator.Tests/LoginControllerTest.cs
+++ Hiof.DotNetCourse.V2023.Group14/APICommunicator.Tests/LoginControllerTest.cs
@@ -1,10 +1,33 @@
 ﻿using System;
+using Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes;
+using Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Security;
+using Hiof.DotNetCourse.V2023.Group14.LoginService.Controllers;
 using Hiof.DotNetCourse.V2023.Group14.UserAccountService;
 using Hiof.DotNetCourse.V2023.Group14.UserAccountService.Controllers;
+using Hiof.DotNetCourse.V2023.Group14.UserAccountService.Data;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+
 namespace APICommunicator.Tests
 {
 	public class LoginControllerTest
 	{
+		/*
+		[Fact]
+		public void TestValidLoginAttempt()
+		{
+			var mock = new Mock<LoginDbContext>(new DbContextOptionsBuilder<LoginDbContext>().Options);
+            var loginModel = new LoginModel { UserName = "stian" };
+            mock.Setup(m => m.LoginModel.Single(l => l.UserName == "stian")).Returns(loginModel);
+
+            var controller = new LoginController(mock.Object);
+
+			var result = controller.VerifyLogin(new LoginInfo("stian", "abc123"));
+
+			Assert.Equal("Login Success", result);
+		}
+		*/
 
 	}
 }
