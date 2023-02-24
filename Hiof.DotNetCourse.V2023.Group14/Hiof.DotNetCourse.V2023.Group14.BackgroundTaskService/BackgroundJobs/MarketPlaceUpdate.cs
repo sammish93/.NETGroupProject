2008421748ef6873f77043ed48c65648829c1a17
stian96 <stiankru@hiof.no>
2023-02-24 20:38:41 +0100

    Created a V1MarketPlacePost class modell to use with the MarketPlaceUpdate background task.

---
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.BackgroundTaskService/BackgroundJobs/MarketPlaceUpdate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Hangfire.Common;
+using Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes.V1;
+using Newtonsoft.Json;
+
+namespace Hiof.DotNetCourse.V2023.Group14.BackgroundTaskService.BackgroundJobs
+{
+	public class MarketPlaceUpdate
+	{
+		private readonly ILogger<MarketPlaceUpdate> _logger;
+		private readonly HttpClient _httpClient;
+
+		public MarketPlaceUpdate(ILogger<MarketPlaceUpdate> logger, HttpClient client)
+		{
+			_logger = logger;
+			_httpClient = client;
+		}
+
+		public void UpdateMarketPlace()
+		{
+			_logger.LogInformation("Background job for updating marketplace every hour");
+		}
+
+		public async Task UpdateMarketPlaceJob()
+		{
+			try
+			{
+				_logger.LogInformation("Updating market place...");
+
+				var response = await _httpClient.GetAsync("/market");
+				if (response.IsSuccessStatusCode)
+				{
+					var content = await response.Content.ReadAsStringAsync();
+					var marketPlacePosts = JsonConvert.DeserializeObject<IEnumerable<V1MarketplacePost>>(content);
+				}
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex, "Error updating marketplace.");
+			}
+		}
+	}
+}
+
---
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.ClassLibrary/Classes/V1/V1MarketplacePost.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes.V1
+{
+	public class V1MarketplacePost
+	{
+        private Guid _id;
+        private string _sellersName;
+        private V1Book _book;
+        private string _condition;
+        private decimal _price;
+        private string _currency;
+        private string _description;
+        private DateTime _dateCreated;
+        private DateTime _dateModified;
+        private string _status;
+        private string _buyersName;
+
+
+		public V1MarketplacePost(string sellerName, V1Book book, string cond, decimal price, string currency,
+                                 string description, DateTime created, DateTime modified, string status, string buyerName)
+		{
+            _id = new Guid();
+            _sellersName = sellerName;
+            _book = book;
+            _condition = cond;
+            _price = price;
+            _currency = currency;
+            _description = description;
+            _dateCreated = created;
+            _dateModified = modified;
+            _status = status;
+            _buyersName = buyerName;
+		}
+
+        [JsonProperty("id")]
+        public Guid Id { get; }
+
+        [JsonProperty("sellersName")]
+        public string? SellersName { get; set; }
+
+        [JsonProperty("book")]
+        public V1Book? Book { get; set; }
+
+        [JsonProperty("condition")]
+        public string? Condition { get; set; }
+
+        [JsonProperty("price")]
+        public decimal Price { get; set; }
+
+        [JsonProperty("currency")]
+        public string? Currency { get; set; }
+
+        [JsonProperty("description")]
+        public string? Description { get; set; }
+
+        [JsonProperty("dateCreated")]
+        public DateTime DateCreated { get; set; }
+
+        [JsonProperty("dateModified")]
+        public DateTime DateModified { get; set; }
+
+        [JsonProperty("status")]
+        public string? Status { get; set; }
+
+        [JsonProperty("buyersName")]
+        public string? BuyersName { get; set; }
+    }
+}
+
