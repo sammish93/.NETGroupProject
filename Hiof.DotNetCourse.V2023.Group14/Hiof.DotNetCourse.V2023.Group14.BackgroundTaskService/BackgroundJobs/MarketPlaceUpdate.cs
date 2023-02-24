67ab66054fb891ffea357f5088baf483a74c33ab
stian96 <stiankru@hiof.no>
2023-02-24 20:49:11 +0100

    Added a description of the MarketPlaceUpdate background job, about what the class does.

--- Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.BackgroundTaskService/BackgroundJobs/MarketPlaceUpdate.cs
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.BackgroundTaskService/BackgroundJobs/MarketPlaceUpdate.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using Hangfire;
 using Hangfire.Common;
 using Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes.V1;
 using Newtonsoft.Json;
 
 namespace Hiof.DotNetCourse.V2023.Group14.BackgroundTaskService.BackgroundJobs
 {
+	/*
+		The class is designed to check for updates in the marketplace, by sending
+		an http-Get request to the '/market' endpoint (not implemented yet). This
+		is to retrieve the current list of posts for sale. It then deserialize the
+		response into a list of 'V1MarketplacePost' objects. 
+	 */
 	public class MarketPlaceUpdate
 	{
 		private readonly ILogger<MarketPlaceUpdate> _logger;
@@ -19,15 +26,16 @@ namespace Hiof.DotNetCourse.V2023.Group14.BackgroundTaskService.BackgroundJobs
 		public void UpdateMarketPlace()
 		{
 			_logger.LogInformation("Background job for updating marketplace every hour");
+			RecurringJob.AddOrUpdate(() => UpdateMarketPlaceJob("/market"), cronExpression: "0 * * * *");
 		}
 
-		public async Task UpdateMarketPlaceJob()
+		public async Task UpdateMarketPlaceJob(string endpoint)
 		{
 			try
 			{
 				_logger.LogInformation("Updating market place...");
 
-				var response = await _httpClient.GetAsync("/market");
+				var response = await _httpClient.GetAsync(endpoint);
 				if (response.IsSuccessStatusCode)
 				{
 					var content = await response.Content.ReadAsStringAsync();
