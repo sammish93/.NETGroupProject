17fd8a3ec4aa696c3d688354fcbaffc837fbae2e
stian96 <stiankru@hiof.no>
2023-02-24 16:14:31 +0100

    Konfigurert Hangfire i Startup.cs fila.

--- Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.BackgroundTaskService/Startup.cs
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.BackgroundTaskService/Startup.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Hangfire;
 using Microsoft.AspNetCore.Builder;
 
 namespace Hiof.DotNetCourse.V2023.Group14.BackgroundTaskService
 {
     public class Startup
     {
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IServiceCollection services)
         {
-            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+            // Set up Hangfire with SQL server storage
+            string connectionString = "<SQL connection string>";
+            GlobalConfiguration.Configuration.UseSqlServerStorage(connectionString);
+
+            // Add Hangfire service
+            services.AddHangfire(x => x.UseSqlServerStorage(connectionString));
+        }
+
+ 
+        public void Configure(IApplicationBuilder application)
+        {
+            application.UseHangfireDashboard();
+
         }
     }
 }
