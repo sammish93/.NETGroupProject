44f4cf34b56e3dd508a0f07987ac0398977ae93b
stian96 <stiankru@hiof.no>
2023-03-01 13:52:06 +0100

    Made a new Proxy Controller.

---
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.ProxyService/Controllers/ProxyController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hiof.DotNetCourse.V2023.Group14.ProxyService.Controllers
+{
+	public class ProxyController : ControllerBase
+	{
+		private readonly HttpClient _httpClient;
+
+		public ProxyController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClient = httpClientFactory.CreateClient();
+		}
+	}
+}
+
--- Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.ProxyService/Program.cs
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.ProxyService/Program.cs
@@ -1,12 +1,12 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddHttpClient();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddHttpClient();
+
 
 var app = builder.Build();
 
