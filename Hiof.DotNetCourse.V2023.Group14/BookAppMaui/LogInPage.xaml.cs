a46dc42c6a8c1f7ccd0156232f1e0220730392e7
a5h71 <ashtim@hiof.no>
2023-03-06 12:47:28 +0100

    Deleted Search page and added login page

---
+++ Hiof.DotNetCourse.V2023.Group14/BookAppMaui/LogInPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace BookAppMaui;
+
+public partial class LogInPage : ContentPage
+{
+	public LogInPage()
+	{
+		InitializeComponent();
+	}
+}
--- Hiof.DotNetCourse.V2023.Group14/BookAppMaui/SearchPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace BookAppMaui;
-
-public partial class NewPage2 : ContentPage
-{
-	public NewPage2()
-	{
-		InitializeComponent();
-	}
-}
