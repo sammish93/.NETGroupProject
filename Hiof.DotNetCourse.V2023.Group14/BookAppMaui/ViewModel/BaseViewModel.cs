1fdcf99ff83598d1111cac61dc26779390fcec85
a5h71 <ashtim@hiof.no>
2023-03-11 12:09:21 +0100

    Added a BaseModel

---
+++ Hiof.DotNetCourse.V2023.Group14/BookAppMaui/ViewModel/BaseViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Hiof.DotNetCourse.V2023.Group14.BookAppMaui.ViewModel
+{
+    public partial class BaseViewModel : ObservableObject
+
+    {
+        public BaseViewModel()
+        {
+
+        }
+
+        [ObservableProperty]
+        
+        bool isBusy;
+        [ObservableProperty]
+        string title;
+
+        public bool IsNotBusy => !IsBusy;
+    }
+}
--- Hiof.DotNetCourse.V2023.Group14/BookAppMaui/ViewModel/LoginViewModel.cs
+++ Hiof.DotNetCourse.V2023.Group14/BookAppMaui/ViewModel/LoginViewModel.cs
@@ -4,10 +4,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes.V1;
+
 
 namespace Hiof.DotNetCourse.V2023.Group14.BookAppMaui.ViewModel
 {
-    class Class1
+    public partial class LoginViewModel : ObservableObject
     {
+        V1LoginModel _loginModel;
+
+        
+        private async void login()
+        {
+
+        }
     }
 }
