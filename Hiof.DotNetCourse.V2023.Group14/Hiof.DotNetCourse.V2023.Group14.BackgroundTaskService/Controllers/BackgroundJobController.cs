bd72b5bc2c4bc1543320764772cf80dceb7720f4
stian96 <stiankru@hiof.no>
2023-03-12 11:51:49 +0100

    Updated backgroundjobController

---
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.BackgroundTaskService/Controllers/BackgroundJobController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Hangfire;
+using Hiof.DotNetCourse.V2023.Group14.BackgroundTaskService.BackgroundJobs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Hiof.DotNetCourse.V2023.Group14.BackgroundTaskService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BackgroundJobController : ControllerBase
+    {
+
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult SendWelcomeMessage(string mail)
+        {
+
+            var jobId = BackgroundJob.Enqueue(() => SendMail(mail));
+
+            return Ok($"Great! The job {jobId} has been completed. The mail has been sent to the user.");
+        }
+
+        public void SendMail(string mail)
+        {
+            Console.WriteLine($"This is a test - Hello {mail}");
+        }
+    }
+
+}
+
