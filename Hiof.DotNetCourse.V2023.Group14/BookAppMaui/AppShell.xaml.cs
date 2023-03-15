517e07be7927e61c2268a82f3a228c31121b11ec
Sam Davies <samuelcd@hiof.no>
2023-03-15 16:22:38 +0100

    added nav bar as well as changed param for AppShell to receive a V1User

--- Hiof.DotNetCourse.V2023.Group14/BookAppMaui/AppShell.xaml.cs
+++ Hiof.DotNetCourse.V2023.Group14/BookAppMaui/AppShell.xaml.cs
@@ -1,17 +1,28 @@
 ﻿using Hiof.DotNetCourse.V2023.Group14.BookAppMaui.View;
+using Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes.V1;
 
 namespace Hiof.DotNetCourse.V2023.Group14.BookAppMaui
 {
     public partial class AppShell : Shell
     {
-        public AppShell()
+        public V1User User;
+        public string Name { get; set; } = "defaultNameThingy";
+        public AppShell(V1User user)
         {
+            User = user;
+            Name = user.FirstName;
+
             InitializeComponent();
 
-            Routing.RegisterRoute(nameof(LogInPage), typeof(LogInPage));
             Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
-            Routing.RegisterRoute(nameof(SignupPage), typeof(SignupPage));
+
+            
+            BindingContext = this;
+
             /*
+            Routing.RegisterRoute(nameof(LogInPage), typeof(LogInPage));
+            Routing.RegisterRoute(nameof(SignupPage), typeof(SignupPage));
+
             var userSaveKey = Preferences.Get("UserIsLoggedIn", false);
 
             if(userSaveKey == true)
@@ -22,8 +33,8 @@ namespace Hiof.DotNetCourse.V2023.Group14.BookAppMaui
             {
                 MyAppShell.CurrentItem = MyLogin;
             }
+            //Routing.RegisterRoute(nameof(Page2), typeof(Page2));
             */
-            Routing.RegisterRoute(nameof(Page2), typeof(Page2));
         }
     }
 }
