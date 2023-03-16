0914c451a65c3d2b5f78c0b703283f3d5aea199c
stian96 <stiankru@hiof.no>
2023-03-16 19:08:15 +0100

    Added a new config folder in useraccountservice, and a new ServiceExtension class to configure Identity.

--- Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.UserAccountService/Data/UserIdentityContext.cs
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.UserAccountService/Data/UserIdentityContext.cs
@@ -1,11 +1,22 @@
 ﻿using System;
+using Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes.V1;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 namespace Hiof.DotNetCourse.V2023.Group14.UserAccountService.Data
 {
-	public class UserIdentityContext
+    // The class inherits from IdentityDbContext, not DbContext, because
+    // we want to integrate our context with identity.
+	public class UserIdentityContext : IdentityDbContext<V1UserIdentity>
 	{
-		public UserIdentityContext()
+		public UserIdentityContext(DbContextOptions options) : base(options)
 		{
 		}
-	}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
 }
 
