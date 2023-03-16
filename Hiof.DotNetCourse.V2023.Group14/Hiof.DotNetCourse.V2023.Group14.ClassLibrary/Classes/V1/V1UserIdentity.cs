5ccd79e85ed8883e8fefb5f3e88bbfa8b8ae6f0d
stian96 <stiankru@hiof.no>
2023-03-16 18:40:26 +0100

    Created a new V1UserIdentity class to test Microsoft Identity.

---
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.ClassLibrary/Classes/V1/V1UserIdentity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes.V1
+{
+	// Class to test the Identity library. I dont want to mess anything
+	// up in V1User, so i made this test class.
+	public class V1UserIdentity : IdentityUser
+	{
+
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+	}
+}
+
