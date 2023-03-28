ab404f810d13ea6d8010688f883049ae329c62dd
a5h71 <ashtim@hiof.no>
2023-03-28 10:36:53 +0200

    Defined the readinggoal class

--- Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.ClassLibrary/Classes/V1/V1ReadingGoals.cs
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.ClassLibrary/Classes/V1/V1ReadingGoals.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes.V1
 {
-    internal class V1ReadingGoals
+    [Table("reading_goals", Schema ="dbo")]
+    public class V1ReadingGoals
     {
+        [Key]
+        [Column("id", TypeName = "char(36")]
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "{0} is required")]
+        [Column("user_id", TypeName = "nvarchar(36)")]
+        public Guid UserId { get; set; }
+        [Column("goal_start", TypeName = "datetime")]
+        public DateTime GoalStartDate { get; set; }
+        [Column("goal_end", TypeName = "datetime")]
+        public DateTime GoalEndDate { get; set;}
+        [Column("goal_target")]
+        public int GoalTarget { get; set; }
+        [Column("curr_target")]
+        public int CurrentTarget { get; set; }
+        [Column("last_updated", TypeName = "datetime")]
+        public DateTime LastUpdated { get; set; }
+
+        public V1ReadingGoals(V1User user, DateTime goalStartDate, DateTime goalEndDate, int goalTarget, int currentTarget)
+        {
+            Id = Guid.NewGuid();
+            UserId = user.Id;
+            GoalStartDate = goalStartDate;
+            GoalEndDate = goalEndDate;
+            GoalTarget = goalTarget;
+            CurrentTarget = currentTarget;
+            LastUpdated = DateTime.UtcNow;
+        }
+
+        public V1ReadingGoals() { }
     }
 }
