15b5ae5a9ae3b3b7120905da5a5758d0b8333d41
a5h71 <ashtim@hiof.no>
2023-03-28 10:27:15 +0200

    Added Reading goals

---
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.ClassLibrary/Classes/V1/V1ReadingGoals.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes.V1
+{
+    internal class V1ReadingGoals
+    {
+    }
+}
---
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.ReadingGoalService/Controllers/V1/ReadingGoalsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hiof.DotNetCourse.V2023.Group14.ReadingGoalService.Controllers.V1
+{
+    public class ReadingGoalsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
