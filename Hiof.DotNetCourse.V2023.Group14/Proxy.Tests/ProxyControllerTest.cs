e9ef96ca59cbc6ac5fb99b0d5a413bdad132ac7b
stian96 <stiankru@hiof.no>
2023-04-01 23:41:05 +0200

    Tried to solve a bug in ProxyControllerTest.

--- Hiof.DotNetCourse.V2023.Group14/Proxy.Tests/ProxyControllerTest.cs
+++ Hiof.DotNetCourse.V2023.Group14/Proxy.Tests/ProxyControllerTest.cs
@@ -1,4 +1,5 @@
-﻿using Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes.V1.Security;
+﻿using Azure;
+using Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes.V1.Security;
 using Hiof.DotNetCourse.V2023.Group14.ProxyService.Configuration;
 using Hiof.DotNetCourse.V2023.Group14.ProxyService.Controllers;
 using Microsoft.AspNetCore.Mvc;
@@ -8,34 +9,39 @@ using Moq.Protected;
 using Newtonsoft.Json;
 using System.Net;
 using System.Text;
+using static System.Net.WebRequestMethods;
 
 namespace Proxy.Tests;
 
 public class ProxyControllerTest
 {
+    private Mock<HttpClient> _mockHttp;
+    private Mock<IHttpClientFactory> _mockHttpFactory;
+    private Mock<IOptions<ProxySettings>> _mockSettings;
+
+    private void SetupMockObjects(String url)
+    {
+        _mockHttp = new Mock<HttpClient>();
+        _mockHttpFactory = new Mock<IHttpClientFactory>();
+        _mockSettings = new Mock<IOptions<ProxySettings>>();
+        _mockSettings.Setup(x => x.Value).Returns(new ProxySettings { LoginVerification = url });
+        _mockHttpFactory.Setup(x => x.CreateClient(It.IsAny<string>())).Returns(_mockHttp.Object);
+    }
 
     [Fact]
     public async Task Verification_ReturnsOkResult_WhenVerificationSucceeds()
     {
-        // Arrange
-        var mockHttp = new Mock<HttpClient>();
-        var mockHttpFactory = new Mock<IHttpClientFactory>();
-        var mockSettings = new Mock<IOptions<ProxySettings>>();
+        // Setup
         var response = new HttpResponseMessage(HttpStatusCode.OK);
-        var url = "https://localhost:7021/api/1.0/login/verification";
+        SetupMockObjects("https://localhost:7021/api/1.0/login/verification");
 
-        mockHttpFactory.Setup(f =>
-        f.CreateClient(It.IsAny<string>())).Returns(mockHttp.Object);
-
-        mockHttp.Setup(c =>
+        _mockHttp.Setup(c =>
         c.SendAsync(
             It.IsAny<HttpRequestMessage>(),
             It.IsAny<CancellationToken>())).ReturnsAsync(response);
 
-        mockSettings.Setup(s =>
-        s.Value).Returns(new ProxySettings{ LoginVerification = url });
 
-        var controller = new V1ProxyController(mockHttpFactory.Object, mockSettings.Object);
+        var controller = new V1ProxyController(_mockHttpFactory.Object, _mockSettings.Object);
         var loginInfo = new V1LoginInfo("testaccount", "TestPassword123!");
 
         // Act
@@ -49,19 +55,15 @@ public class ProxyControllerTest
     [Fact]
     public async Task Verification_ReturnsBadRequest_WhenVerificationFails()
     {
-        // Arrange
-        var mockHttp = new Mock<HttpClient>();
-        var mockSettings = new Mock<IOptions<ProxySettings>>();
+        // Setup
         var loginInfo = new V1LoginInfo("fail", "s");
-        var url = "https://localhost:7021/api/1.0/login/verification";
+        SetupMockObjects("https://localhost:7021/api/1.0/login/verification");
 
-        mockHttp.Setup(c => c.SendAsync(
+        _mockHttp.Setup(c => c.SendAsync(
             It.IsAny<HttpRequestMessage>(),
             It.IsAny<CancellationToken>())).ReturnsAsync(new HttpResponseMessage(HttpStatusCode.BadRequest));
 
-        mockSettings.Setup(s => s.Value).Returns(new ProxySettings { LoginVerification = url });
-
-        var controller = new V1ProxyController(Mock.Of<IHttpClientFactory>(), mockSettings.Object);
+        var controller = new V1ProxyController(_mockHttpFactory.Object, _mockSettings.Object);
 
         // Act
         var result = await controller.Verification(loginInfo);
@@ -69,6 +71,31 @@ public class ProxyControllerTest
         // Assert
         var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
         Assert.Equal((int)HttpStatusCode.BadRequest, badRequestResult.StatusCode);
+    }
+
+    // TODO: Fix the test, it does not work right now.
+    [Fact]
+    public async Task GetAll_ReturnsOkResult_WhenGetUsersSucceeds()
+    {
+        // Arrange
+        var mockHttpFactory = new Mock<IHttpClientFactory>();
+        var client = mockHttpFactory.Object.CreateClient();
+        var mockClient = new Mock<HttpClient>();
+
+        var settings = new Mock<IOptions<ProxySettings>>();
+        var url = "https://localhost:7021/api/1.0/users/getUsers";
+
+        mockHttpFactory.Setup(x => x.CreateClient(It.IsAny<string>())).Returns(mockClient.Object);
+
+        settings.Setup(x => x.Value).Returns(new ProxySettings { GetUsers = url });
+
+        var controller = new V1ProxyController(mockHttpFactory.Object, settings.Object);
+
+        // Act
+        var result = await controller.GetAll();
+
+        // Assert
+        Assert.IsType<OkObjectResult>(result);
     }
 
 }
