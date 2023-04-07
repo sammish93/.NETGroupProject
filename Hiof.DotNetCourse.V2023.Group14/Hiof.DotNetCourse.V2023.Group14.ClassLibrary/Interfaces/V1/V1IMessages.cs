829b9d05972c05ef66f58ed89cecd82d9bffcaa2
stian96 <stiankru@hiof.no>
2023-04-07 17:04:57 +0200

    Created a service folder and a V1MessagingService class that implements the V1IMessage interface.

--- Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.ClassLibrary/Interfaces/V1/V1IMessages.cs
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.ClassLibrary/Interfaces/V1/V1IMessages.cs
@@ -3,6 +3,9 @@ using Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes.V1.MessageModels;
 
 namespace Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Interfaces.V1
 {
+	/// <summary>
+	/// Contract for the V1MessagingController API.
+	/// </summary>
 	public interface V1IMessages
 	{
 		Task<IEnumerable<V1ConversationModel>> GetByParticipant(string participant);
---
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.MessagingService/Services/V1MessagingService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes.V1.MessageModels;
+using Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Interfaces.V1;
+
+namespace Hiof.DotNetCourse.V2023.Group14.MessagingService.Services
+{
+	public class V1MessagingService : V1IMessages
+	{
+		public V1MessagingService()
+		{
+		}
+
+        public Task AddMessageToConversation(Guid conversationId, Guid messageId, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateNewConversation(Guid conversationId, IEnumerable<string> participants)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteConversation(Guid conversationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteMessage(Guid conversationId, Guid messageId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<V1ConversationModel> GetByConversationId(Guid participantId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<V1ConversationModel>> GetByParticipant(string participant)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateExistingMessage(Guid conversationId, Guid messageId, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
