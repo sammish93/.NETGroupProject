eb3569163b40bb4f76796269cf3712d544da98d5
stian96 <stiankru@hiof.no>
2023-04-07 18:26:21 +0200

    Modified Program.cs and added V1MessagingService in the scope, added controllers and appsettings.json file.

--- Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.MessagingService/Program.cs
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.MessagingService/Program.cs
@@ -1,26 +1,41 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿
+using Hiof.DotNetCourse.V2023.Group14.MessagingService.Services;
 
-// Add services to the container.
+namespace Hiof.DotNetCourse.V2023.Group14.MessagingService;
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddScoped<V1MessagingService>();
+        builder.Services.AddControllers();
+        builder.Configuration.AddJsonFile("appsettings.json");
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
 
-app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
 
