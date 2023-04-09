4e069e481aba3ed96a186a00dfcd18c56b0b90df
stian96 <stiankru@hiof.no>
2023-04-09 15:14:07 +0200

    .

--- Hiof.DotNetCourse.V2023.Group14/MessagingTests/MessagingServiceTest.cs
+++ Hiof.DotNetCourse.V2023.Group14/MessagingTests/MessagingServiceTest.cs
@@ -1,10 +1,15 @@
-﻿namespace MessagingTests;
+﻿using Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes.V1.MessageModels;
+using Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Interfaces.V1;
+using Hiof.DotNetCourse.V2023.Group14.MessagingService.Controllers;
+using Hiof.DotNetCourse.V2023.Group14.MessagingService.Data;
+using Hiof.DotNetCourse.V2023.Group14.MessagingService.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace MessagingTests;
 
 public class MessagingServiceTest
 {
-    [Fact]
-    public void Test1()
-    {
-
-    }
 }
