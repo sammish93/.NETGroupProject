d390cc2c95767a2809f72a641c405be4a69b3af1
stian96 <stiankru@hiof.no>
2023-04-16 14:48:17 +0200

    Added isRead boolean variable in V1Mesages entity and made a new migration and updated the database.

--- Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.ClassLibrary/Classes/V1/MessageModels/V1Messages.cs
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.ClassLibrary/Classes/V1/MessageModels/V1Messages.cs
@@ -22,6 +22,8 @@ namespace Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes.V1.MessageModels
 
         public List<V1Reactions> Reactions { get; set; }
 
+        public bool isRead { get; set; }
+
         [JsonIgnore]
         public Guid ConversationId { get; set; }
 
