c2629c6811bd881b3c9325b10ad3fc26544f691d
Sam Davies <samuelcd@hiof.no>
2023-04-22 11:11:40 +0200

    added marketplacepage to maui project with viewmodel

---
+++ Hiof.DotNetCourse.V2023.Group14/BookAppMaui/View/MarketplacePage.xaml.cs
@@ -0,0 +1,139 @@
+using Hiof.DotNetCourse.V2023.Group14.BookAppMaui.ViewModel;
+using Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes.V1;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Hiof.DotNetCourse.V2023.Group14.BookAppMaui.View;
+
+public partial class MarketplacePage : ContentPage
+{
+    public MarketplacePage()
+    {
+        InitializeComponent();
+        this.BindingContext = new MarketplacePageViewModel();
+        
+    }
+
+
+
+    protected override async void OnAppearing()
+    {
+        var model = BindingContext as MarketplacePageViewModel;
+
+        if (model != null)
+        {
+            await model.LoadAsync();
+        }
+        base.OnAppearing();
+    }
+
+    protected override void OnSizeAllocated(double width, double height)
+    {
+        base.OnSizeAllocated(width, height);
+        dynamicColumn.WidthRequest = width;
+        dynamicColumn.HeightRequest = height;
+    }
+
+    private async void OnItemSelected(object sender, SelectionChangedEventArgs e)
+    {
+        var model = BindingContext as ViewModel.MarketplacePageViewModel;
+
+        if (model != null)
+        {
+            if (!e.CurrentSelection.IsNullOrEmpty() && e.CurrentSelection.First() != null)
+            {
+                V1LibraryEntry entry = ((V1LibraryEntry)e.CurrentSelection.First());
+
+                string isbn = "";
+                if (!entry.LibraryEntryISBN13.IsNullOrEmpty()) 
+                {
+                    isbn = entry.LibraryEntryISBN13;
+                } else if (!entry.LibraryEntryISBN10.IsNullOrEmpty())
+                {
+                    isbn = entry.LibraryEntryISBN10;
+                }
+
+                string thumbnailUrl = "";
+
+                V1Book book = await model.GetBookWithEntryAsync(isbn);
+                thumbnailUrl = book.ImageLinks["thumbnail"];
+
+                var entryWithImage = new V1LibraryEntryWithImage(entry, thumbnailUrl);
+                model.SelectedEntry = entryWithImage;
+                model.SelectedEntryBook = book;
+
+                var absoluteBanner = this.FindByName<AbsoluteLayout>("absoluteBanner");
+                var selectPromptLabel = this.FindByName<Label>("selectPromptLabel");
+                var entryDatePicker = this.FindByName<DatePicker>("entryDatePicker");
+                var entryFormGrid = this.FindByName<Grid>("entryFormGrid");
+
+                selectPromptLabel.IsVisible = false;
+                absoluteBanner.IsVisible = true;
+                model.PopulateSelectedEntryFields();
+                entryDatePicker.Date = (DateTime)model.SelectedDate;
+                entryFormGrid.IsVisible = true;
+            }
+        }
+    }
+
+    private void RadioButtonAll_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        var collectionLibraryAll = this.FindByName<CollectionView>("collectionLibraryAll");
+        var collectionLibraryRead = this.FindByName<CollectionView>("collectionLibraryRead");
+        var collectionLibraryReading = this.FindByName<CollectionView>("collectionLibraryReading");
+        var collectionLibraryToRead = this.FindByName<CollectionView>("collectionLibraryToRead");
+
+        collectionLibraryAll.IsVisible = true;
+        collectionLibraryRead.IsVisible = false;
+        collectionLibraryReading.IsVisible = false;
+        collectionLibraryToRead.IsVisible = false;
+    }
+
+    private void RadioButtonRead_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        var collectionLibraryAll = this.FindByName<CollectionView>("collectionLibraryAll");
+        var collectionLibraryRead = this.FindByName<CollectionView>("collectionLibraryRead");
+        var collectionLibraryReading = this.FindByName<CollectionView>("collectionLibraryReading");
+        var collectionLibraryToRead = this.FindByName<CollectionView>("collectionLibraryToRead");
+
+        collectionLibraryAll.IsVisible = false;
+        collectionLibraryRead.IsVisible = true;
+        collectionLibraryReading.IsVisible = false;
+        collectionLibraryToRead.IsVisible = false;
+    }
+
+    private void RadioButtonReading_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        var collectionLibraryAll = this.FindByName<CollectionView>("collectionLibraryAll");
+        var collectionLibraryRead = this.FindByName<CollectionView>("collectionLibraryRead");
+        var collectionLibraryReading = this.FindByName<CollectionView>("collectionLibraryReading");
+        var collectionLibraryToRead = this.FindByName<CollectionView>("collectionLibraryToRead");
+
+        collectionLibraryAll.IsVisible = false;
+        collectionLibraryRead.IsVisible = false;
+        collectionLibraryReading.IsVisible = true;
+        collectionLibraryToRead.IsVisible = false;
+    }
+
+    private void RadioButtonToRead_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        var collectionLibraryAll = this.FindByName<CollectionView>("collectionLibraryAll");
+        var collectionLibraryRead = this.FindByName<CollectionView>("collectionLibraryRead");
+        var collectionLibraryReading = this.FindByName<CollectionView>("collectionLibraryReading");
+        var collectionLibraryToRead = this.FindByName<CollectionView>("collectionLibraryToRead");
+
+        collectionLibraryAll.IsVisible = false;
+        collectionLibraryRead.IsVisible = false;
+        collectionLibraryReading.IsVisible = false;
+        collectionLibraryToRead.IsVisible = true;
+    }
+
+    private void entryDatePicker_DateSelected(object sender, DateChangedEventArgs e)
+    {
+        var model = BindingContext as ViewModel.MarketplacePageViewModel;
+
+        if (model != null)
+        {
+            model.UpdateDate(e.NewDate);
+        }
+    }
+}
