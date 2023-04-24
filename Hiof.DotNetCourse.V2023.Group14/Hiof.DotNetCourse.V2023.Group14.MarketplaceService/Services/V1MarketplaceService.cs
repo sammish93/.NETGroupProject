b3c17867796c92cb4578a93b6429d80cbdc9c573
stian96 <stiankru@hiof.no>
2023-04-24 22:01:15 +0200

    Implemented method for creating a new marketplace post in the V1Marketplace service.

--- Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.MarketplaceService/Services/V1MarketplaceService.cs
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.MarketplaceService/Services/V1MarketplaceService.cs
@@ -2,14 +2,52 @@
 using Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Interfaces.V1;
 using Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes.V1.MarketplaceModels;
 using Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes.V1;
+using Hiof.DotNetCourse.V2023.Group14.MarketplaceService.Data;
 
 namespace Hiof.DotNetCourse.V2023.Group14.MarketplaceService.Services
 {
     public class V1MarketplaceService : V1IMarketplace
     {
-        public Task CreateNewPost(V1MarketplaceBook post)
+        private readonly MarketplaceContext _context;
+
+        public V1MarketplaceService(MarketplaceContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
+        }
+
+        public async Task CreateNewPost(V1MarketplaceBook post)
+        {
+            var newPost = new V1MarketplaceBook
+            {
+                Id = Guid.NewGuid(),
+                Condition = post.Condition,
+                Price = post.Price,
+                Currency = post.Currency,
+                Status = post.Status,
+                OwnerId = post.OwnerId,
+                DateCreated = new DateTime(),
+                DateModified = new DateTime()
+            };
+
+            // Find user with the given owner id
+            var user = await _context.MarketplaceUser.FindAsync(post.OwnerId);
+
+            if (user == null)
+            {
+                // Create new marketplace user
+                user = new V1MarketplaceUser
+                {
+                    OwnerId = post.OwnerId,
+                    Posts = new List<V1MarketplaceBook>()
+                };
+                // Add the new user to database. 
+                await _context.MarketplaceUser.AddAsync(user);
+            }
+
+            // Add the new post to the users Posts list.
+            user.Posts.Add(newPost);
+            await _context.MarketplaceBooks.AddAsync(newPost);
+            await _context.SaveChangesAsync();
         }
 
         public Task<bool> DeletePost(Guid id)
