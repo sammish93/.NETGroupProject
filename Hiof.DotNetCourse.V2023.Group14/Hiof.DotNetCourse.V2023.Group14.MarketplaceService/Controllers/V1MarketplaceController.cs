16a5a39e1d00e5607b74e796d9552575da61b8c2
stian96 <stiankru@hiof.no>
2023-04-24 19:13:27 +0200

    created interface for the marketplace controller.

---
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.MarketplaceService/Controllers/V1MarketplaceController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hiof.DotNetCourse.V2023.Group14.MarketplaceService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class V1MarketplaceController : ControllerBase
+{
+   
+}
+
