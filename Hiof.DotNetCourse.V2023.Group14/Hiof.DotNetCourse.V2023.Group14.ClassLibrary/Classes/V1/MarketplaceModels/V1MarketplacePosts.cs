d6ce88c27b36bff2c6a45705df22a7a1c792d360
stian96 <stiankru@hiof.no>
2023-04-24 20:48:55 +0200

    created a new db-context for marketplace service.

---
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.ClassLibrary/Classes/V1/MarketplaceModels/V1MarketplacePosts.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes.V1.MarketplaceModels
+{
+	public class V1MarketplacePosts
+	{
+		public Guid OwnerId { get; set; }
+
+		public List<V1MarketplaceBook> Books { get; set; }
+	}
+}
+
---
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.MarketplaceService/Data/MarketplaceContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes.V1;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hiof.DotNetCourse.V2023.Group14.MarketplaceService.Data
+{
+	public class MarketplaceContext : DbContext
+	{
+		public MarketplaceContext(DbContextOptions<MarketplaceContext> options): base(options)
+		{
+		}
+
+		public DbSet<V1MarketplacePost> MarketplacePost { get; set; }
+
+		public DbSet<V1MarketplaceBook> MarketplaceBooks { get; set; }
+	}
+}
+
