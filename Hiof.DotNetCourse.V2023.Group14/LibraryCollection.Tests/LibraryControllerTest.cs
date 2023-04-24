3927d4140315783c64bbb0cce0407d88fea1180e
Sam Davies <samuelcd@hiof.no>
2023-04-24 12:10:33 +0000

    Updated LibraryControllerTest.cs

--- Hiof.DotNetCourse.V2023.Group14/LibraryCollection.Tests/LibraryControllerTest.cs
+++ Hiof.DotNetCourse.V2023.Group14/LibraryCollection.Tests/LibraryControllerTest.cs
@@ -122,7 +122,7 @@ namespace LibraryCollection.Tests
 
 
             Assert.IsType<OkObjectResult>(actionResult);
-            Assert.Equal(entry4.UserId.ToString(), userId);
+            Assert.Equal(entry5.UserId.ToString(), userId);
         }
 
         [Fact]
