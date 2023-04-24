e027673cb1817a85245181d155ae808e7434f888
stian96 <stiankru@hiof.no>
2023-04-24 21:45:34 +0200

    Created new migration which fixed the foreignkey problem.

--- Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.MarketplaceService/Migrations/20230424194355_FixForeignKey.Designer.cs
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.MarketplaceService/Migrations/20230424194355_FixForeignKey.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Hiof.DotNetCourse.V2023.Group14.MarketplaceService.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Hiof.DotNetCourse.V2023.Group14.MarketplaceService.Migrations
 {
     [DbContext(typeof(MarketplaceContext))]
-    partial class MarketplaceContextModelSnapshot : ModelSnapshot
+    [Migration("20230424194355_FixForeignKey")]
+    partial class FixForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.MarketplaceService/Migrations/20230424194355_FixForeignKey.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hiof.DotNetCourse.V2023.Group14.MarketplaceService.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixForeignKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_marketplace_books_marketplace_posts_V1MarketplaceUserOwnerId",
+                schema: "dbo",
+                table: "marketplace_books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_marketplace_books_V1MarketplaceUserOwnerId",
+                schema: "dbo",
+                table: "marketplace_books");
+
+            migrationBuilder.DropColumn(
+                name: "V1MarketplaceUserOwnerId",
+                schema: "dbo",
+                table: "marketplace_books");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_marketplace_books_OwnerId",
+                schema: "dbo",
+                table: "marketplace_books",
+                column: "OwnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_marketplace_owner",
+                schema: "dbo",
+                table: "marketplace_books",
+                column: "OwnerId",
+                principalSchema: "dbo",
+                principalTable: "marketplace_posts",
+                principalColumn: "OwnerId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_marketplace_owner",
+                schema: "dbo",
+                table: "marketplace_books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_marketplace_books_OwnerId",
+                schema: "dbo",
+                table: "marketplace_books");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "V1MarketplaceUserOwnerId",
+                schema: "dbo",
+                table: "marketplace_books",
+                type: "char(36)",
+                nullable: true,
+                collation: "ascii_general_ci");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_marketplace_books_V1MarketplaceUserOwnerId",
+                schema: "dbo",
+                table: "marketplace_books",
+                column: "V1MarketplaceUserOwnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_marketplace_books_marketplace_posts_V1MarketplaceUserOwnerId",
+                schema: "dbo",
+                table: "marketplace_books",
+                column: "V1MarketplaceUserOwnerId",
+                principalSchema: "dbo",
+                principalTable: "marketplace_posts",
+                principalColumn: "OwnerId");
+        }
+    }
+}
