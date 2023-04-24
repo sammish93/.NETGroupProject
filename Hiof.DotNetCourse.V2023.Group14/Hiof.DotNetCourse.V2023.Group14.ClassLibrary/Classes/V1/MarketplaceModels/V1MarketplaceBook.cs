3d3c6d9d88a3e70bccdf8b756f0f1b457840c2b3
stian96 <stiankru@hiof.no>
2023-04-24 21:08:40 +0200

    Added key, columns, foreignkey and datatypes on all properties in V1MarketplaceBook.

--- Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.ClassLibrary/Classes/V1/MarketplaceModels/V1MarketplaceBook.cs
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.ClassLibrary/Classes/V1/MarketplaceModels/V1MarketplaceBook.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Enums.V1;
 
 namespace Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes.V1
@@ -6,17 +8,37 @@ namespace Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes.V1
 	/// <summary>
 	/// Model class representing a book in the marketplace service.
 	/// </summary>
+	[Table("marketplace_books", Schema="dbo")]
 	public class V1MarketplaceBook
 	{
 
+		[Key]
+		[Column("Id", TypeName = "char(36)")]
 		public Guid Id { get; set; }
+
+		[Column("Condition", TypeName = "varchar(255)")]
 		public string Condition { get; set; }
+
+		[Column("Price", TypeName = "decimal(3, 2)")]
 		public decimal Price { get; set; }
+
+		[Column("Currency", TypeName = "int")]
+		[EnumDataType(typeof(V1Currency))]
 		public V1Currency Currency { get; set; }
+
+		[Column("Status", TypeName = "int")]
+		[EnumDataType(typeof(V1BookStatus))]
 		public V1BookStatus Status { get; set; }
+
+		[ForeignKey("OwnerId")]
+		[Column("OwnerId", TypeName = "char(36)")]
 		public Guid OwnerId { get; set; }
-		public DateTime dateCreated { get; set; }
-		public DateTime dateModified { get; set; }
+
+		[Column("DateCreated", TypeName = "datetime")]
+		public DateTime DateCreated { get; set; }
+
+		[Column("DateModified", TypeName = "datetime")]
+		public DateTime DateModified { get; set; }
 
 	}
 }
--- Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.ClassLibrary/Classes/V1/MarketplaceModels/V1MarketplacePosts.cs
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.ClassLibrary/Classes/V1/MarketplaceModels/V1MarketplacePosts.cs
@@ -1,11 +1,20 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes.V1.MarketplaceModels
 {
+	/// <summary>
+	/// Model class representing a user in the marketplace and his/her´s posts.
+	/// </summary>
+	[Table("marketplace_posts", Schema="dbo")]
 	public class V1MarketplacePosts
 	{
+		[Key]
+		[Column("OwnerId", TypeName= "char(36)")]
 		public Guid OwnerId { get; set; }
 
-		public List<V1MarketplaceBook> Books { get; set; }
+		public List<V1MarketplaceBook> Posts { get; set; }
 	}
 }
 
