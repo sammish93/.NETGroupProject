3432043d82c8cbb40bb38e45fa5dc355397f121e
stian96 <stiankru@hiof.no>
2023-04-24 19:13:37 +0200

    Deleted default files.

---
+++ Hiof.DotNetCourse.V2023.Group14/Hiof.DotNetCourse.V2023.Group14.ClassLibrary/Interfaces/V1/V1IMarketplace.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes.V1;
+
+namespace Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Interfaces.V1
+{
+	/// <summary>
+	/// Contract for the V1Marketplace controller.
+	/// </summary>
+	public interface V1IMarketplace
+	{
+		Task<V1Marketplace> GetAllPosts();
+		Task<V1Marketplace> GetPostById(Guid id);
+		Task CreateNewPost(V1Marketplace post);
+		Task UpdatePost(Guid id, V1Marketplace post);
+		Task<bool> DeletePost(Guid id);
+	}
+}
+
