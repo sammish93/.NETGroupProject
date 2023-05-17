8226c967201c8afe5ad1161f6865c3e9819f7fb5
stian96 <stiankru@hiof.no>
2023-05-17 18:55:40 +0200

    added a db context mock in the test class.

--- Hiof.DotNetCourse.V2023.Group14/MarketplaceTests/MarketplaceControllerTest.cs
+++ Hiof.DotNetCourse.V2023.Group14/MarketplaceTests/MarketplaceControllerTest.cs
@@ -2,7 +2,11 @@
 using Moq;
 namespace MarketplaceTests;
 
+using Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes.V1;
+using Hiof.DotNetCourse.V2023.Group14.ClassLibrary.Classes.V1.MarketplaceModels;
+using Hiof.DotNetCourse.V2023.Group14.MarketplaceService.Data;
 using Hiof.DotNetCourse.V2023.Group14.MarketplaceService.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 public class MarketPlaceControllerTest
@@ -11,15 +15,28 @@ public class MarketPlaceControllerTest
     private readonly V1MarketplaceController _controller;
     private readonly Mock<ILogger<V1MarketplaceController>> _loggerMock;
     private readonly Mock<V1MarketplaceService> _serviceMock;
+    private readonly Mock<MarketplaceContext> _contextMock;
 
     // Initialize the objects.
     public MarketPlaceControllerTest()
     {
         _loggerMock = new Mock<ILogger<V1MarketplaceController>>();
-        _serviceMock = new Mock<V1MarketplaceService>();
+        _serviceMock = new Mock<V1MarketplaceService>(_contextMock);
         _controller = new V1MarketplaceController(_loggerMock.Object, _serviceMock.Object);
     }
 
 
-    // TODO: Write first test.
+    [Fact]
+    public async Task GetAllPosts_ReturnsNotFoundResult_WhenNoPostsExist()
+    {
+        // Arrange.
+        _serviceMock.Setup(service => service.GetAllPosts()).ReturnsAsync(new List<V1MarketplaceBookResponse>());
+
+        // Act.
+        var result = await _controller.GetAllPosts();
+
+        // Assert.
+        Assert.IsType<NotFoundObjectResult>(result);
+
+    }
 }
