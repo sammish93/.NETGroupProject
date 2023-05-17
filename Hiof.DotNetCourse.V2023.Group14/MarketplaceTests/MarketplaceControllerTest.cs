248168f0fc7b8a39c0255d6a9729e0e41ad89f16
stian96 <stiankru@hiof.no>
2023-05-17 18:41:51 +0200

    Implemented setup for marketplace controller in the test class.

--- Hiof.DotNetCourse.V2023.Group14/MarketplaceTests/MarketplaceControllerTest.cs
+++ Hiof.DotNetCourse.V2023.Group14/MarketplaceTests/MarketplaceControllerTest.cs
@@ -1,8 +1,24 @@
-﻿namespace MarketplaceTests;
+﻿using Hiof.DotNetCourse.V2023.Group14.MarketplaceService.Controllers;
+using Moq;
+namespace MarketplaceTests;
+
+using Hiof.DotNetCourse.V2023.Group14.MarketplaceService.Services;
+using Microsoft.Extensions.Logging;
 
 public class MarketPlaceControllerTest
 {
-    // TODO: Set up configuration.
+    // Setup and mock objects. 
+    private readonly V1MarketplaceController _controller;
+    private readonly Mock<ILogger<V1MarketplaceController>> _loggerMock;
+    private readonly Mock<V1MarketplaceService> _serviceMock;
+
+    // Initialize the objects.
+    public MarketPlaceControllerTest()
+    {
+        _loggerMock = new Mock<ILogger<V1MarketplaceController>>();
+        _serviceMock = new Mock<V1MarketplaceService>();
+        _controller = new V1MarketplaceController(_loggerMock.Object, _serviceMock.Object);
+    }
 
 
     // TODO: Write first test.
