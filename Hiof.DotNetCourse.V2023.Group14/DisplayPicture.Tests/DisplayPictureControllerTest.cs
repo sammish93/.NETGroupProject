e0a21d2fd383e0007c09eadf3fb9efb6b5f09eea
stian96 <stiankru@hiof.no>
2023-05-18 19:32:42 +0200

    implemented mocking of all the dependencies in the DisplayPictureControllerTest.

--- Hiof.DotNetCourse.V2023.Group14/DisplayPicture.Tests/DisplayPictureControllerTest.cs
+++ Hiof.DotNetCourse.V2023.Group14/DisplayPicture.Tests/DisplayPictureControllerTest.cs
@@ -1,7 +1,21 @@
-﻿namespace DisplayPicture.Tests;
+﻿using Hiof.DotNetCourse.V2023.Group14.UserDisplayPictureService.Controllers;
+using Hiof.DotNetCourse.V2023.Group14.UserDisplayPictureService.Data;
+using Hiof.DotNetCourse.V2023.Group14.UserDisplayPictureService.Services;
+using Moq;
+
+namespace DisplayPicture.Tests;
 
 public class DisplayPictureControllerTest
 {
+    private readonly V1DisplayPictureController _controller;
+    private readonly Mock<V1UserIconService> _serviceMock;
+    private readonly Mock<UserIconContext> _contextMock;
 
+    public DisplayPictureControllerTest()
+    {
+        _serviceMock = new Mock<V1UserIconService>();
+        _contextMock = new Mock<UserIconContext>();
+        _controller = new V1DisplayPictureController(_serviceMock.Object, _contextMock.Object);
+    }
     
 }
