5f03c5044ba4945d5e8b63aaf04d4490ed9986e5
stian96 <stiankru@hiof.no>
2023-05-18 19:41:17 +0200

    added using Microsoft.AspNetCore.Mvc package in test project.

--- Hiof.DotNetCourse.V2023.Group14/DisplayPicture.Tests/DisplayPictureControllerTest.cs
+++ Hiof.DotNetCourse.V2023.Group14/DisplayPicture.Tests/DisplayPictureControllerTest.cs
@@ -1,6 +1,7 @@
 ﻿using Hiof.DotNetCourse.V2023.Group14.UserDisplayPictureService.Controllers;
 using Hiof.DotNetCourse.V2023.Group14.UserDisplayPictureService.Data;
 using Hiof.DotNetCourse.V2023.Group14.UserDisplayPictureService.Services;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 
 namespace DisplayPicture.Tests;
@@ -16,6 +17,16 @@ public class DisplayPictureControllerTest
         _serviceMock = new Mock<V1UserIconService>();
         _contextMock = new Mock<UserIconContext>();
         _controller = new V1DisplayPictureController(_serviceMock.Object, _contextMock.Object);
+    }
+
+    [Fact]
+    public async Task GetById_ReturnsBadRequestResult_WhenIdIsEmpty()
+    {
+        // Arrange.
+        var id = Guid.Empty;
+        _serviceMock.Setup(service => service.GetById(id));
+
+        // Act.
     }
     
 }
